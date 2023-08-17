73343e099f1e67d7dbafe8fd3bbdbfabdda70680
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2023-08-17 14:01:53 -0400

    app finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,193 @@
-import Image from 'next/image'
+"use client";
 
+import { montserrat } from "./layout";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Check } from "lucide-react";
+interface PricingProps {
+  id: number;
+  title: string;
+}
+interface PlanProps {
+  id: number;
+  title: string;
+  price: string;
+  plan: string;
+  features: string[];
+}
+
+const pricing: PricingProps[] = [
+  {
+    id: 1,
+    title: "Monthly",
+  },
+  {
+    id: 2,
+    title: "Quarterly (save 10%)",
+  },
+];
+
+const plans: PlanProps[] = [
+  {
+    id: 1,
+    title:
+      "Ideal for those who've already got their website up and running and are seeking assistance to enhance it further.",
+    price: "$2500",
+    plan: "Pro",
+    features: [
+      "3-5 day turnaround",
+      "Native Development",
+      "Task delivered one-by-one",
+      "Dedicated dashboard",
+      "Updates via Dashboard & Slack",
+    ],
+  },
+  {
+    id: 2,
+    title:
+      "Ideal if you want to build or scale your website fast, with the strategy calls included.",
+    price: "$3800",
+    plan: "Pro Plus",
+    features: [
+      "1-3 day turnaround",
+      "Monthly strategy call",
+      "Commercial license",
+      "Native Development",
+      "Tasks delivered one-by-one",
+      "Dedicated dashboard",
+      "Updates via Dashboard & Slack",
+    ],
+  },
+  {
+    id: 3,
+    title:
+      "If these plans don't fit, let's create one that suits. Customize your subscription for a perfect fit, bigger or smaller!",
+    price: "Let's talk",
+    plan: "Custom",
+    features: [
+      "Everything in design & development",
+      "Strategy workshop",
+      "Priority support",
+      "Multiple tasks at once",
+      "Ongoing autonomous A/B testing",
+      "Advanced custom development",
+    ],
+  },
+];
 export default function Home() {
+  const [activeTab, setActiveTab] = useState(1);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="bg-gradient-to-b from-[#e8dffa] to-[#f8f4fb] lg:h-screen p-10 lg:px-20 lg:py-10 ">
+      <div className="flex  justify-center flex-col items-center ">
+        <div className=" flex items-center text-center flex-col w-full max-w-xl">
+          <div
+            className={`text-primary font-semibold text-4xl pb-4 ${montserrat.className}`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Choose your right plan!
+          </div>
+          <div className="text-secondary ">
+            Select from the best plans, ensuring a perfect match. Need more or
+            less?{" "}
+          </div>
+          <div className="text-secondary">
+            Customize your subscription for a seamless fit!
+          </div>
+          <div className="flex flex-row w-full justify-center bg-white rounded-full space-x-1  mt-8 h-12 px-1 py-1">
+            {pricing.map((item) => {
+              const isActive = item.id === activeTab;
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => setActiveTab(item.id)}
+                  className={`${
+                    isActive ? "text-white" : "hover:opacity-80"
+                  } relative  text-sm font-medium text-secondary  transition focus-visible:outline-2 w-full `}
+                >
+                  {isActive && (
+                    <motion.div
+                      layoutId="bubble"
+                      className="absolute inset-0 rounded-full bg-primary  px-0.5 "
+                      style={{ borderRadius: 9999 }}
+                      transition={{
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.6,
+                      }}
+                    />
+                  )}
+                  <span className="relative">{item.title}</span>
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6 ">
+        {plans.map((item) => {
+          const isFirstTwoPlans = [1, 2].includes(item.id);
+          return (
+            <div
+              key={item.id}
+              className={`${
+                isFirstTwoPlans
+                  ? "bg-white border"
+                  : "bg-gradient-to-b from-[#e8dffa] to-[#fff] border-primary border"
+              } flex rounded-xl p-6 flex-col gap-3 `}
+            >
+              <div className="">
+                <span
+                  className={`${
+                    !isFirstTwoPlans ? "bg-white text-primary" : "bg-primary"
+                  }  py-2 px-4 rounded-xl`}
+                >
+                  {item.plan}
+                </span>
+              </div>
+
+              <p className="text-secondary text-xs">{item.title}</p>
+              <div className="flex flex-row items-end">
+                <span
+                  className={`text-primary font-semibold text-4xl  ${montserrat.className}`}
+                >
+                  {item.price}
+                </span>
+                {isFirstTwoPlans && (
+                  <span className="text-secondary text-xs ml-2">/month</span>
+                )}
+              </div>
+              <span
+                className={`${
+                  isFirstTwoPlans ? "" : "border-primary"
+                } border border-b-0  `}
+              />
+
+              <span
+                className={`${montserrat.className} flex-grow font-semibold text-primary text-xs`}
+              >
+                {item.features.map((feature) => (
+                  <div key={feature} className="flex flex-row pb-2 gap-1">
+                    <Check className="text-primary" size={16} />
+                    <span className="text-primary">{feature}</span>
+                  </div>
+                ))}
+              </span>
+              <div className="place-items-end w-full">
+                <motion.button
+                  whileTap={{ scale: 0.9 }}
+                  className={`${
+                    isFirstTwoPlans
+                      ? "border text-primary "
+                      : "bg-secondary text-white"
+                  } h-10 rounded-xl text-xs font-semibold cursor-pointer w-full`}
+                >
+                  {isFirstTwoPlans ? "Get Started" : "Book a Call"}
+                </motion.button>
+              </div>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    //! DO NOT ADD ANYTHING HERE BECAUSE IT WILL DISABLED ALL THE DEFAULT TAILWIND CLASSES, ADD ONLY THE EXTEND
     extend: {
+      colors: {
+        primary: "#2f2b43",
+        secondary: "#6d697a",
+      },
+      backgroundColor: {
+        primary: "#8249ff",
+        secondary: "#2f2b43",
+      },
+      borderColor: {
+        primary: "#d0c6e7",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
