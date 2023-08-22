119127f587237a96d5ffcda6b3be7b59a6f55d19
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2023-08-22 10:13:18 -0400

    fixed layout issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,11 +78,11 @@ const plans: PlanProps[] = [
 export default function Home() {
   const [activeTab, setActiveTab] = useState(1);
   return (
-    <div className="bg-gradient-to-b from-[#e8dffa] to-[#f8f4fb] lg:h-screen p-10 lg:px-20 lg:py-10 ">
-      <div className="flex  justify-center flex-col items-center ">
-        <div className=" flex items-center text-center flex-col w-full max-w-xl">
+    <div className="bg-gradient-to-b from-[#e8dffa] to-[#f8f4fb] p-10 lg:h-screen lg:px-20 lg:py-10 ">
+      <div className="flex  flex-col items-center justify-center ">
+        <div className=" flex w-full max-w-xl flex-col items-center text-center">
           <div
-            className={`text-primary font-semibold text-4xl pb-4 ${montserrat.className}`}
+            className={`pb-4 text-4xl font-semibold text-primary ${montserrat.className}`}
           >
             Choose your right plan!
           </div>
@@ -93,7 +93,7 @@ export default function Home() {
           <div className="text-secondary">
             Customize your subscription for a seamless fit!
           </div>
-          <div className="flex flex-row w-full justify-center bg-white rounded-full space-x-1  mt-8 h-12 px-1 py-1">
+          <div className="mt-8 flex h-12 w-full flex-row justify-center space-x-1  rounded-full bg-white px-1 py-1">
             {pricing.map((item) => {
               const isActive = item.id === activeTab;
               return (
@@ -102,7 +102,7 @@ export default function Home() {
                   onClick={() => setActiveTab(item.id)}
                   className={`${
                     isActive ? "text-white" : "hover:opacity-80"
-                  } relative  text-sm font-medium text-secondary  transition focus-visible:outline-2 w-full `}
+                  } relative  w-full text-sm font-medium  text-secondary transition focus-visible:outline-2 `}
                 >
                   {isActive && (
                     <motion.div
@@ -124,7 +124,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6 md:grid-cols-3 ">
+      <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 ">
         {plans.map((item) => {
           const isFirstTwoPlans = [1, 2].includes(item.id);
           return (
@@ -132,30 +132,30 @@ export default function Home() {
               key={item.id}
               className={`${
                 isFirstTwoPlans
-                  ? "bg-white border"
-                  : "bg-gradient-to-b from-[#e8dffa] to-[#fff] border-primary border"
-              } flex rounded-xl p-6 flex-col gap-3 `}
+                  ? "border bg-white"
+                  : "border border-primary bg-gradient-to-b from-[#e8dffa] to-[#fff]"
+              } flex flex-col gap-3 rounded-xl p-6`}
             >
               <div className="">
                 <span
                   className={`${
                     !isFirstTwoPlans ? "bg-white text-primary" : "bg-primary"
-                  }  py-2 px-4 rounded-xl`}
+                  }  rounded-xl px-4 py-2`}
                 >
                   {item.plan}
                 </span>
               </div>
-              <div className="lg:h-10 md:h-12">
-                <p className="text-secondary text-xs">{item.title}</p>
+              <div className="sm:h-12 md:h-12 lg:h-10">
+                <p className="text-xs text-secondary">{item.title}</p>
               </div>
               <div className="flex flex-row items-end">
                 <span
-                  className={`text-primary font-semibold text-4xl  ${montserrat.className}`}
+                  className={`text-4xl font-semibold text-primary  ${montserrat.className}`}
                 >
                   {item.price}
                 </span>
                 {isFirstTwoPlans && (
-                  <span className="text-secondary text-xs ml-2">/month</span>
+                  <span className="ml-2 text-xs text-secondary">/month</span>
                 )}
               </div>
               <span
@@ -165,23 +165,23 @@ export default function Home() {
               />
 
               <span
-                className={`${montserrat.className} flex-grow font-semibold text-primary text-xs`}
+                className={`${montserrat.className} flex-grow text-xs font-semibold text-primary`}
               >
                 {item.features.map((feature) => (
-                  <div key={feature} className="flex flex-row pb-2 gap-1">
+                  <div key={feature} className="flex flex-row gap-1 pb-2">
                     <Check className="text-primary" size={16} />
                     <span className="text-primary">{feature}</span>
                   </div>
                 ))}
               </span>
-              <div className="place-items-end w-full">
+              <div className="w-full place-items-end">
                 <motion.button
                   whileTap={{ scale: 0.9 }}
                   className={`${
                     isFirstTwoPlans
                       ? "border text-primary "
                       : "bg-secondary text-white"
-                  } h-10 rounded-xl text-xs font-semibold cursor-pointer w-full`}
+                  } h-10 w-full cursor-pointer rounded-xl text-xs font-semibold`}
                 >
                   {isFirstTwoPlans ? "Get Started" : "Book a Call"}
                 </motion.button>
